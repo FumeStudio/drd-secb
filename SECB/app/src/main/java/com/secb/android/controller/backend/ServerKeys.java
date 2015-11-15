7ef090c6d8460c5782e06d70d810a653fa38bb4e
josephfathy <josephfathy1991@gmail.com>
2015-11-16 00:34:44 +0200

    Done : -Flurry Integrated -Serverkeys :Server url (production , development).

--- SECB/app/src/main/java/com/secb/android/controller/backend/ServerKeys.java
+++ SECB/app/src/main/java/com/secb/android/controller/backend/ServerKeys.java
@@ -18,56 +18,61 @@ public class ServerKeys
 	public static final String LOGEN_KEY_USER_NAME = "username";
 	public static final String LOGEN_KEY_PASSWORD = "password";
 
+	public static String PRODUCTION_URL = "http://secb.linkdev.com/";
+	public static String DEV_URL = "http://secb.linkdev.com/";
+	public static boolean isProduction = true;
+	public static String serverUrl = isProduction?PRODUCTION_URL:DEV_URL;
+
 	/*Photo Gallery*/
-	public static final String PHOTO_URL ="http://secb.linkdev.com/webapi/api/photos/getphotos" ;
+	public static final String PHOTO_URL =serverUrl+"webapi/api/photos/getphotos" ;
 
 	/*Video Gallery*/
-	public static final String VIDEO_URL = "http://secb.linkdev.com/webapi/api/Videos/GetVideos";
+	public static final String VIDEO_URL = serverUrl+"webapi/api/Videos/GetVideos";
 
 	/*News Categories*/
-	public static final String NEWS_CATEGORIES_URL = "http://secb.linkdev.com/webapi/api/News/GetNewsCategories";
+	public static final String NEWS_CATEGORIES_URL = serverUrl+"webapi/api/News/GetNewsCategories";
 
 	/*News List*/
-	public static final String NEWS_URL = "http://secb.linkdev.com/webapi/api/News/GetNews";
+	public static final String NEWS_URL = serverUrl+"webapi/api/News/GetNews";
 
 	/*Events Categories*/
-	public static final String EVENTS_CATEGORIES_URL = "http://secb.linkdev.com/webapi/api/events/GetEventsCategories";
+	public static final String EVENTS_CATEGORIES_URL = serverUrl+"webapi/api/events/GetEventsCategories";
 
 	/*Events List*/
-	public static final String EVENTS_URL = "http://secb.linkdev.com/webapi/api/events/GetEvents";
+	public static final String EVENTS_URL = serverUrl+"webapi/api/events/GetEvents";
 
 	/*Events Cities*/
-	public static final String EVENTS_CITIES_URL = "http://secb.linkdev.com/webapi/api/events/GetCities";
+	public static final String EVENTS_CITIES_URL =serverUrl+ "webapi/api/events/GetCities";
 
 	/*Forget Password*/
-	public static final String FORGET_PASSWORD = "http://secb.linkdev.com/webapi/api/user/ForgetPassword";
+	public static final String FORGET_PASSWORD = serverUrl+"webapi/api/user/ForgetPassword";
 
 	/*E-guide Location Types*/
-	public static final String EGUIDE_LOCATION_TYPES = "http://secb.linkdev.com/webapi/api/location/GetlocationTypes";
+	public static final String EGUIDE_LOCATION_TYPES = serverUrl+"webapi/api/location/GetlocationTypes";
 
 	/*E-guide Location List*/
-	public static final String EGUIDE_LOCATION_LIST= "http://secb.linkdev.com/WebApi/api/location/GetLocation";
+	public static final String EGUIDE_LOCATION_LIST= "WebApi/api/location/GetLocation";
 
 	/*E-guide Location Details List*/
-	public static final String EGUIDE_LOCATION_DETAILS_LIST= "http://secb.linkdev.com/WebApi/api/location/GetLocationByID";
+	public static final String EGUIDE_LOCATION_DETAILS_LIST= serverUrl+"WebApi/api/location/GetLocationByID";
 
 	/*E-guide Organizers List*/
-	public static final String EGUIDE_ORGANIZERS_LIST= "http://secb.linkdev.com/WebApi/api/Organizer/Getorganizer";
+	public static final String EGUIDE_ORGANIZERS_LIST=serverUrl+ "WebApi/api/Organizer/Getorganizer";
 
 	/*E-Services RequestList*/
-	public static final String E_Services_REQUESTS_LIST = "http://secb.linkdev.com/WebApi/api/Requests/GetRequests";
+	public static final String E_Services_REQUESTS_LIST = serverUrl+"WebApi/api/Requests/GetRequests";
 
 	/*E-Services RequestTypes*/
-	public static final String E_Services_REQUESTS_TYPES_LIST = "http://secb.linkdev.com/WebApi/api/Requests/GetRequesttypes";
+	public static final String E_Services_REQUESTS_TYPES_LIST =serverUrl+ "WebApi/api/Requests/GetRequesttypes";
 
 	/*E-Services WorkSpaceModes*/
-	public static final String E_Services_Work_SPACE_MODES_LIST = "http://secb.linkdev.com/WebApi/api/Requests/GetWorkSpaceModes";
+	public static final String E_Services_Work_SPACE_MODES_LIST = serverUrl+"WebApi/api/Requests/GetWorkSpaceModes";
 
 	/*E-Services Statistics List*/
-	public static final String E_Services_STATISTICS_LIST = "http://secb.linkdev.com/WebApi/api/requests/GetUserTasksStatistics";
+	public static final String E_Services_STATISTICS_LIST = serverUrl+"WebApi/api/requests/GetUserTasksStatistics";
 
 	/*Contact Us*/
-	public static final String CONTACT_US = "http://secb.linkdev.com/webapi/api/user/ContactUs";
+	public static final String CONTACT_US = serverUrl+"webapi/api/user/ContactUs";
 
 
 }
