83052412a9a4e076cb3e07fdeaf9afe4e830b3a1
josephfathy <josephfathy1991@gmail.com>
2015-11-17 00:55:52 +0200

    feedbacks table done , except first 6 items

--- SECB/app/src/main/java/com/secb/android/view/components/LayoutAnimator.java
+++ SECB/app/src/main/java/com/secb/android/view/components/LayoutAnimator.java
@@ -5,140 +5,191 @@ import android.animation.ObjectAnimator;
 import android.os.Handler;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+import com.secb.android.R;
+
+import net.comptoirs.android.common.view.CTApplication;
 
 public class LayoutAnimator {
-    private final float startPointY;
-    private final float startPointX;
-    ViewGroup layout;
+	private final float startPointY;
+	private final float startPointX;
+	ViewGroup layout;
 	View darkArea;
-    public boolean isDown;
-    public boolean isVerticalAnimation;
-    String animationDirection = "translationY";
+	public boolean isDown;
+	public boolean isVerticalAnimation;
+	String animationDirection = "translationY";
 
-    public LayoutAnimator(ViewGroup layout, boolean isVerticalAnimation, float startPointX, float startPointY)
-    {
-        this.layout = layout;
-        this.isVerticalAnimation = isVerticalAnimation;
-        animationDirection = isVerticalAnimation ? "translationY" : "translationX" ;
-        this.startPointX=startPointX;
-        this.startPointY=startPointY;
-        hideFirst();
+	final Animation fadeOut = AnimationUtils.loadAnimation(CTApplication.getContext(), R.anim.fadeout);
+	final Animation fadeIn = AnimationUtils.loadAnimation(CTApplication.getContext(), R.anim.fade_in);
+
+	public LayoutAnimator(ViewGroup layout, boolean isVerticalAnimation, float startPointX, float startPointY) {
+		this.layout = layout;
+		this.isVerticalAnimation = isVerticalAnimation;
+		animationDirection = isVerticalAnimation ? "translationY" : "translationX";
+		this.startPointX = startPointX;
+		this.startPointY = startPointY;
+		hideFirst();
 //	    darkArea = layout.findViewById(R.id.layout_dark_layer);
-    }
-	public  void setDarkLayer(View _darkLayer){
-		this.darkArea=_darkLayer;
 	}
-    public void hidePreviewPanel() {
 
-        android.animation.Animator.AnimatorListener listener = new android.animation.Animator.AnimatorListener() {
+	public void setDarkLayer(View _darkLayer) {
+		this.darkArea = _darkLayer;
+	}
 
-            @Override
-            public void onAnimationStart(Animator animator) {
-			//hide dark area
-	            if(darkArea!=null)
-		            darkArea.setVisibility(View.INVISIBLE);
-            }
+	public void hidePreviewPanel() {
+		fadeOut();
+		android.animation.Animator.AnimatorListener listener = new android.animation.Animator.AnimatorListener() {
 
-            @Override
-            public void onAnimationEnd(Animator animator) {
-                layout.setVisibility(View.GONE);
-                isDown = true;
-            }
+			@Override
+			public void onAnimationStart(Animator animator) {
+/*				//hide dark area
+				if (darkArea != null)
+					darkArea.setVisibility(View.INVISIBLE);*/
 
-            @Override
-            public void onAnimationCancel(Animator animator) {
+			}
 
-            }
+			@Override
+			public void onAnimationEnd(Animator animator) {
+				layout.setVisibility(View.GONE);
+				isDown = true;
+			}
 
-            @Override
-            public void onAnimationRepeat(Animator animator) {
+			@Override
+			public void onAnimationCancel(Animator animator) {
 
-            }
-        };
+			}
 
-        ObjectAnimator animator=null;
-        if(isVerticalAnimation)
-            animator = ObjectAnimator.ofFloat(layout, animationDirection, layout.getMeasuredHeight())
-                .setDuration(400);
-        else
-            animator = ObjectAnimator.ofFloat(layout, animationDirection, layout.getMeasuredWidth())
-                    .setDuration(400);
+			@Override
+			public void onAnimationRepeat(Animator animator) {
 
+			}
+		};
 
-        animator.addListener(listener);
-        animator.start();
-
-    }
-
-    public void showPreviewPanel() {
-
-        android.animation.Animator.AnimatorListener listener = new android.animation.Animator.AnimatorListener() {
-
-            @Override
-            public void onAnimationStart(Animator animator) {
-
-                new Handler().postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        layout.setVisibility(View.VISIBLE);
-                    }
-                }, 300);
-
-            }
-
-            @Override
-            public void onAnimationEnd(Animator animator) {
-                layout.setVisibility(View.VISIBLE);
-                isDown = false;
-	            //Show dark area
-	            if(darkArea!=null)
-		            darkArea.setVisibility(View.VISIBLE);
-	            }
-
-            @Override
-            public void onAnimationCancel(Animator animator) {
-
-            }
-
-            @Override
-            public void onAnimationRepeat(Animator animator) {
-
-            }
-        };
-        ObjectAnimator animator =null;
-        if(isVerticalAnimation)
-            animator = ObjectAnimator.ofFloat(layout, animationDirection, -(0 /*layout.getMeasuredHeight()*/))
-                .setDuration(850);
-        else
-            animator = ObjectAnimator.ofFloat(layout, animationDirection, -(0 /*layout.getMeasuredHeight()*/))
-                    .setDuration(850);
-
-        animator.addListener(listener);
-        animator.start();
-
-    }
-
-    public void hideFirst()
-    {
-        layout.setX(startPointX);
-        layout.setY(startPointY);
-        if(isVerticalAnimation){
-            if (!isDown)
-            {
-                ObjectAnimator animatorTemp =
-                        ObjectAnimator.ofFloat(layout, "translationY", layout.getMeasuredHeight())
-                                .setDuration(1);
-                animatorTemp.start();
-            }
-        }
-        else{
-            ObjectAnimator animatorTemp =
-                    ObjectAnimator.ofFloat(layout, "translationX", layout.getMeasuredWidth())
-                            .setDuration(1);
-            animatorTemp.start();
-        }
+		ObjectAnimator animator = null;
+		if (isVerticalAnimation)
+			animator = ObjectAnimator.ofFloat(layout, animationDirection, layout.getMeasuredHeight())
+					.setDuration(400);
+		else
+			animator = ObjectAnimator.ofFloat(layout, animationDirection, layout.getMeasuredWidth())
+					.setDuration(400);
 
 
-    }
+		animator.addListener(listener);
+		animator.start();
 
+	}
+
+	public void showPreviewPanel() {
+
+		android.animation.Animator.AnimatorListener listener = new android.animation.Animator.AnimatorListener() {
+
+			@Override
+			public void onAnimationStart(Animator animator) {
+
+				new Handler().postDelayed(new Runnable() {
+					@Override
+					public void run() {
+						layout.setVisibility(View.VISIBLE);
+					}
+				}, 300);
+
+			}
+
+			@Override
+			public void onAnimationEnd(Animator animator) {
+				layout.setVisibility(View.VISIBLE);
+				isDown = false;
+				fadeIn();
+/*				//Show dark area
+				if (darkArea != null)
+					darkArea.setVisibility(View.VISIBLE);*/
+			}
+
+			@Override
+			public void onAnimationCancel(Animator animator) {
+
+			}
+
+			@Override
+			public void onAnimationRepeat(Animator animator) {
+
+			}
+		};
+		ObjectAnimator animator = null;
+		if (isVerticalAnimation)
+			animator = ObjectAnimator.ofFloat(layout, animationDirection, -(0 /*layout.getMeasuredHeight()*/))
+					.setDuration(850);
+		else
+			animator = ObjectAnimator.ofFloat(layout, animationDirection, -(0 /*layout.getMeasuredHeight()*/))
+					.setDuration(850);
+
+		animator.addListener(listener);
+		animator.start();
+
+	}
+
+	public void hideFirst() {
+		layout.setX(startPointX);
+		layout.setY(startPointY);
+		if (isVerticalAnimation) {
+			if (!isDown) {
+				ObjectAnimator animatorTemp =
+						ObjectAnimator.ofFloat(layout, "translationY", layout.getMeasuredHeight())
+								.setDuration(1);
+				animatorTemp.start();
+			}
+		} else {
+			ObjectAnimator animatorTemp =
+					ObjectAnimator.ofFloat(layout, "translationX", layout.getMeasuredWidth())
+							.setDuration(1);
+			animatorTemp.start();
+		}
+
+
+	}
+
+
+	public void fadeOut() {
+		if (darkArea != null) {
+			fadeOut.setAnimationListener(new Animation.AnimationListener() {
+				@Override
+				public void onAnimationStart(Animation animation) {
+				}
+
+				@Override
+				public void onAnimationRepeat(Animation animation) {
+				}
+
+				@Override
+				public void onAnimationEnd(Animation animation) {
+					darkArea.setVisibility(View.INVISIBLE);
+				}
+			});
+			darkArea.startAnimation(fadeOut);
+		}
+	}
+
+	public void fadeIn() {
+		if (darkArea != null) {
+			darkArea.setVisibility(View.INVISIBLE);
+			fadeIn.setAnimationListener(new Animation.AnimationListener() {
+				@Override
+				public void onAnimationStart(Animation animation) {
+				}
+
+				@Override
+				public void onAnimationRepeat(Animation animation) {
+				}
+
+				@Override
+				public void onAnimationEnd(Animation animation) {
+					darkArea.setVisibility(View.VISIBLE);
+				}
+			});
+			darkArea.startAnimation(fadeIn);
+		}
+	}
 }
+
--- SECB/app/src/main/java/com/secb/android/view/fragments/NewsListFragment.java
+++ SECB/app/src/main/java/com/secb/android/view/fragments/NewsListFragment.java
@@ -36,165 +36,164 @@ import net.comptoirs.android.common.helper.Logger;
 import java.util.ArrayList;
 
 public class NewsListFragment extends SECBBaseFragment
-        implements FragmentBackObserver, View.OnClickListener ,RecyclerCustomClickListener, RequestObserver
+		implements FragmentBackObserver, View.OnClickListener, RecyclerCustomClickListener, RequestObserver
 
 {
 	private static final String TAG = "NewsListFragment";
 	RecyclerView newsRecyclerView;
-    NewsItemRecyclerAdapter newsItemRecyclerAdapter;
-    ArrayList<NewsItem> newsList;
-    NewsFilterData newsFilterData;
+	NewsItemRecyclerAdapter newsItemRecyclerAdapter;
+	ArrayList<NewsItem> newsList;
+	NewsFilterData newsFilterData;
 
-    View view;
+	View view;
 	TextView txtv_noData;
-    private NewsFilterLayout newsFilterLayout=null;
+	private NewsFilterLayout newsFilterLayout = null;
 	private ProgressDialog progressDialog;
 
-    public static NewsListFragment newInstance() {
-        NewsListFragment fragment = new NewsListFragment();
-        return fragment;
-    }
+	public static NewsListFragment newInstance() {
+		NewsListFragment fragment = new NewsListFragment();
+		return fragment;
+	}
 
-    @Override
-    public void onResume()
-    {
-        super.onResume();
-        ((SECBBaseActivity) getActivity()).addBackObserver(this);
-        ((SECBBaseActivity) getActivity()).setHeaderTitleText(getString(R.string.news));
-        ((SECBBaseActivity) getActivity()).disableHeaderBackButton();
-        ((SECBBaseActivity) getActivity()).enableHeaderMenuButton();
-        ((SECBBaseActivity) getActivity()).showFilterButton(true);
-        ((SECBBaseActivity) getActivity()).setApplyFilterClickListener(this);
-        ((SECBBaseActivity) getActivity()).setClearFilterClickListener(this);
+	@Override
+	public void onResume() {
+		super.onResume();
+		((SECBBaseActivity) getActivity()).addBackObserver(this);
+		((SECBBaseActivity) getActivity()).setHeaderTitleText(getString(R.string.news));
+		((SECBBaseActivity) getActivity()).disableHeaderBackButton();
+		((SECBBaseActivity) getActivity()).enableHeaderMenuButton();
+		((SECBBaseActivity) getActivity()).showFilterButton(true);
+		((SECBBaseActivity) getActivity()).setApplyFilterClickListener(this);
+		((SECBBaseActivity) getActivity()).setClearFilterClickListener(this);
 
-    }
+	}
 
-    @Override
-    public void onPause() {
-        super.onPause();
-        ((SECBBaseActivity) getActivity()).removeBackObserver(this);
-        ((SECBBaseActivity) getActivity()).showFilterButton(false);
-    }
+	@Override
+	public void onStop() {
+		super.onStop();
+	}
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
+	@Override
+	public void onPause() {
+		super.onPause();
+		((SECBBaseActivity) getActivity()).removeBackObserver(this);
+		((SECBBaseActivity) getActivity()).showFilterButton(false);
+	}
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        if (view != null) {
-            ViewParent oldParent = (ViewParent) view.getRootView();
-            if (oldParent != container && oldParent != null)
-            {
-                ((ViewGroup) oldParent).removeView(view);
-            }
-        }
-        else {
-            view = LayoutInflater.from(getActivity()).inflate(R.layout.news_list_fragment, container, false);
-            handleButtonsEvents();
-            applyFonts();
-        }
-	    ((MainActivity)getActivity()).setNewsRequstObserver(this);
-        initViews(view);
-        initFilterLayout();
-	    getData();
-        return view;
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		if (view != null) {
+			ViewParent oldParent = (ViewParent) view.getRootView();
+			if (oldParent != container && oldParent != null) {
+				((ViewGroup) oldParent).removeView(view);
+			}
+		} else {
+			view = LayoutInflater.from(getActivity()).inflate(R.layout.news_list_fragment, container, false);
+			handleButtonsEvents();
+			applyFonts();
+		}
+		((MainActivity) getActivity()).setNewsRequstObserver(this);
+		initViews(view);
+		initFilterLayout();
+		getData();
+		return view;
+	}
 
 	@Override
 	public void onDestroyView() {
 		super.onDestroyView();
 		newsRecyclerView.setAdapter(null);
+		((SECBBaseActivity) getActivity()).hideFilterLayout();
 	}
 
-    public void initFilterLayout()
-    {
-        newsFilterLayout= new NewsFilterLayout(getActivity());
-        ((SECBBaseActivity) getActivity()).setFilterLayout(newsFilterLayout ,true);
-        ((SECBBaseActivity) getActivity()).setFilterLayoutView(newsFilterLayout.getLayoutView());
-    }
-    private void handleButtonsEvents() {
-    }
+	public void initFilterLayout() {
+		newsFilterLayout = new NewsFilterLayout(getActivity());
+		((SECBBaseActivity) getActivity()).setFilterLayout(newsFilterLayout, true);
+		((SECBBaseActivity) getActivity()).setFilterLayoutView(newsFilterLayout.getLayoutView());
+	}
 
-    /*
-     * Apply Fonts
-     */
-    private void applyFonts() {
+	private void handleButtonsEvents() {
+	}
 
-	    if(txtv_noData!=null)
-	    {
-		    UiEngine.applyCustomFont(txtv_noData, UiEngine.Fonts.HVAR);
-	    }
+	/*
+	 * Apply Fonts
+	 */
+	private void applyFonts() {
+
+		if (txtv_noData != null) {
+			UiEngine.applyCustomFont(txtv_noData, UiEngine.Fonts.HVAR);
+		}
 //		UiEngine.applyCustomFont(((TextView) view.findViewById(R.id.textViewAbout)), UiEngine.Fonts.HELVETICA_NEUE_LT_STD_CN);
-    }
+	}
 
-    private void goBack() {
-        ((SECBBaseActivity) getActivity()).finishFragmentOrActivity(getClass().getName(),true);
-    }
+	private void goBack() {
+		((SECBBaseActivity) getActivity()).finishFragmentOrActivity(getClass().getName(), true);
+	}
 
-    // ////////////////////////////////////////////////////////////
+	// ////////////////////////////////////////////////////////////
 
-    @Override
-    public void onBack() {
-        goBack();
-    }
+	@Override
+	public void onBack() {
+		goBack();
+	}
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId())
-        {
-            case R.id.imageViewBackHeader:
-                onBack();
-                break;
-            case R.id.btn_applyFilter:
-                applyFilters();
-                break;
-            case R.id.btn_clearFilter:
-                clearFilters();
-                break;
-            default:
-                break;
-        }
-    }
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+			case R.id.imageViewBackHeader:
+				onBack();
+				break;
+			case R.id.btn_applyFilter:
+				applyFilters();
+				break;
+			case R.id.btn_clearFilter:
+				clearFilters();
+				break;
+			default:
+				break;
+		}
+	}
 
 
-	private void applyFilters()
-    {
-        newsFilterData =this.newsFilterLayout.getFilterData();
-	    ((SECBBaseActivity)getActivity()).hideFilterLayout();
-        if(newsFilterData !=null){
-          /*  ((SECBBaseActivity) getActivity()).displayToast("Filter Data \n Time From: "+ newsFilterData.timeFrom+"\n" +
+	private void applyFilters() {
+		newsFilterData = this.newsFilterLayout.getFilterData();
+		((SECBBaseActivity) getActivity()).hideFilterLayout();
+		if (newsFilterData != null) {
+	      /*  ((SECBBaseActivity) getActivity()).displayToast("Filter Data \n Time From: "+ newsFilterData.timeFrom+"\n" +
                     " Time To: "+ newsFilterData.timeTo+" \n" +
                     " Type: "+ newsFilterData.selectedCategoryId+
                     " Selected Category: "+ newsFilterData.newsCategory);*/
-	        startNewsListOperation(newsFilterData,true);
-        }
+			startNewsListOperation(newsFilterData, true);
+		}
 
 
-    }
+	}
 
 	private void clearFilters() {
 		this.newsFilterLayout.clearFilters();
 
 	}
 
-    private void initViews(View view)
-    {
+	private void initViews(View view) {
 //        newsList = DevData.getNewsList();
-	    progressDialog = CustomProgressDialog.getInstance(getActivity(),true);
-	    progressDialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
-		    @Override
-		    public void onCancel(DialogInterface dialog) {
-			    bindViews();
-		    }
-	    });
-        newsRecyclerView = (RecyclerView) view.findViewById(R.id.newsRecyclerView);
-	    txtv_noData = (TextView) view.findViewById(R.id.txtv_noData);
+		progressDialog = CustomProgressDialog.getInstance(getActivity(), true);
+		progressDialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+			@Override
+			public void onCancel(DialogInterface dialog) {
+				bindViews();
+			}
+		});
+		newsRecyclerView = (RecyclerView) view.findViewById(R.id.newsRecyclerView);
+		txtv_noData = (TextView) view.findViewById(R.id.txtv_noData);
 //        newsRecyclerView.addItemDecoration(new DividerItemDecoration(getActivity(), DividerItemDecoration.VERTICAL_LIST));
 		LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
-        newsRecyclerView.setLayoutManager(linearLayoutManager);
-        newsRecyclerView.addOnItemTouchListener(new RecyclerCustomItemTouchListener(getActivity(), newsRecyclerView, this));
+		newsRecyclerView.setLayoutManager(linearLayoutManager);
+		newsRecyclerView.addOnItemTouchListener(new RecyclerCustomItemTouchListener(getActivity(), newsRecyclerView, this));
 //		newsRecyclerView.addOnScrollListener(new EndlessRecyclerOnScrollListener(linearLayoutManager) {
 //			@Override
 //			public void onLoadMore(int current_page) {
@@ -204,42 +203,35 @@ public class NewsListFragment extends SECBBaseFragment
 
 	}
 
-	public void bindViews()
-	{
-		if(newsList!=null && newsList.size()>0)
-		{
+	public void bindViews() {
+		if (newsList != null && newsList.size() > 0) {
 			newsRecyclerView.setVisibility(View.VISIBLE);
 			txtv_noData.setVisibility(View.GONE);
 			newsItemRecyclerAdapter = new NewsItemRecyclerAdapter(getActivity(), newsList);
 			newsRecyclerView.setAdapter(newsItemRecyclerAdapter);
-		}
-		else {
+		} else {
 			newsRecyclerView.setVisibility(View.GONE);
 			txtv_noData.setVisibility(View.VISIBLE);
 			txtv_noData.setText(getString(R.string.news_no_news));
 		}
 	}
 
-	public void getData()
-	{
+	public void getData() {
 		//if news list is loaded in the manager get it and bind
 		//if not and the main activity is still loading the news list
-			// wait for it and it will notify handleRequestFinished in this fragment.
+		// wait for it and it will notify handleRequestFinished in this fragment.
 		//if the main activity finished loading news list and the manager is still empty
-			//start operation here.
+		//start operation here.
 
 
 		newsList = (ArrayList<NewsItem>) NewsManager.getInstance().getNewsUnFilteredList(getActivity());
-		if(newsList!= null && newsList.size()>0){
+		if (newsList != null && newsList.size() > 0) {
 			handleRequestFinished(RequestIds.NEWS_LIST_REQUEST_ID, null, newsList);
-		}
-		else
-		{
+		} else {
 			if (((MainActivity) getActivity()).isNewsLoadingFinished == false) {
 				startWaiting();
-			}
-			else{
-				startNewsListOperation(new NewsFilterData(),true);
+			} else {
+				startNewsListOperation(new NewsFilterData(), true);
 			}
 		}
 
@@ -247,61 +239,50 @@ public class NewsListFragment extends SECBBaseFragment
 
 
 	private void startWaiting() {
-		if(progressDialog!=null&& !progressDialog.isShowing())
-		{
+		if (progressDialog != null && !progressDialog.isShowing()) {
 			progressDialog.show();
 		}
 	}
 
 	private void stopWaiting() {
-		if(progressDialog!=null&& progressDialog.isShowing())
-		{
+		if (progressDialog != null && progressDialog.isShowing()) {
 			progressDialog.dismiss();
 		}
 	}
-	private void startNewsListOperation(NewsFilterData newsFilterData ,boolean showDialog)
-	{
-		NewsListOperation operation = new NewsListOperation(RequestIds.NEWS_LIST_REQUEST_ID,showDialog,getActivity(),newsFilterData,100,0);
+
+	private void startNewsListOperation(NewsFilterData newsFilterData, boolean showDialog) {
+		NewsListOperation operation = new NewsListOperation(RequestIds.NEWS_LIST_REQUEST_ID, showDialog, getActivity(), newsFilterData, 100, 0);
 		operation.addRequsetObserver(this);
 		operation.execute();
 	}
 
 	@Override
-	public void onItemClicked(View v, int position)
-	{
+	public void onItemClicked(View v, int position) {
 		((MainActivity) getActivity()).openNewDetailsFragment(newsList.get(position));
 	}
 
 	@Override
-    public void onItemLongClicked(View v, int position)
-    {
+	public void onItemLongClicked(View v, int position) {
 
-    }
+	}
 
 
 	@Override
-	public void handleRequestFinished(Object requestId, Throwable error, Object resultObject)
-	{
+	public void handleRequestFinished(Object requestId, Throwable error, Object resultObject) {
 		stopWaiting();
-		if (error == null)
-		{
+		if (error == null) {
 			Logger.instance().v(TAG, "Success \n\t\t" + resultObject);
-			if((int)requestId == RequestIds.NEWS_LIST_REQUEST_ID && resultObject!=null){
-				newsList= (ArrayList<NewsItem>) resultObject;
+			if ((int) requestId == RequestIds.NEWS_LIST_REQUEST_ID && resultObject != null) {
+				newsList = (ArrayList<NewsItem>) resultObject;
 				bindViews();
 			}
 
-		}
-		else if (error != null && error instanceof CTHttpError)
-		{
-			Logger.instance().v(TAG,error);
+		} else if (error != null && error instanceof CTHttpError) {
+			Logger.instance().v(TAG, error);
 			int statusCode = ((CTHttpError) error).getStatusCode();
-			if (RequestHandler.isRequestTimedOut(statusCode))
-			{
+			if (RequestHandler.isRequestTimedOut(statusCode)) {
 				ErrorDialog.showMessageDialog(getString(R.string.attention), getString(R.string.timeout), getActivity());
-			}
-			else if (statusCode == -1)
-			{
+			} else if (statusCode == -1) {
 				ErrorDialog.showMessageDialog(getString(R.string.attention), getString(R.string.conn_error),
 						getActivity());
 			}
